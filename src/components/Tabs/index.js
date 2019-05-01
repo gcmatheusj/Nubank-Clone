6c5264f4d48443ac854e7d0d6e4992c612369a5e
gcmatheusj <goca1377@gmail.com>
2019-05-01 20:51:07 -0300

    first commit

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container, TabsContainer, TabText, TabItem,
+} from './styles';
+
+export default function Tabs({ translateY }) {
+  return (
+    <Container
+      style={{
+        transform: [
+          {
+            translateY: translateY.interpolate({
+              inputRange: [0, 390],
+              outputRange: [0, 30],
+              extrapolate: 'clamp',
+            }),
+          },
+        ],
+        opacity: translateY.interpolate({
+          inputRange: [0, 390],
+          outputRange: [1, 0],
+          extrapolate: 'clamp',
+        }),
+      }}
+    >
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#FFF" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#FFF" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#FFF" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
+}
---
+++ src/pages/Main/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
+
+export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  );
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 390 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 390 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 390],
+                    outputRange: [-30, 0, 390],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 256.654.899,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 150.000,00 recebida de Jhones Santos Lira hoje às 18h03
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+      </Content>
+
+      <Tabs translateY={translateY} />
+    </Container>
+  );
+}
